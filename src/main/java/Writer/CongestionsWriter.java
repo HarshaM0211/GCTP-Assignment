0fe7dccfc3aaca053aafb6c12b309265e09f7096
harsha <mukthapuramharsha@gmail.com>
2024-01-10 16:14:52 +0530

    [Refactor] Combined both methods into One

--- src/main/java/Writer/CongestionsWriter.java
+++ src/main/java/Writer/CongestionsWriter.java
@@ -29,97 +29,86 @@ public class CongestionsWriter {
     private static final Logger LOGGER = LoggerFactory.getLogger(CongestionsWriter.class);
 
     // Output filepaths
-    final String fromSignalOutputPath = "outputs/FromSignalsCount.xlsx";
-    final String toSignalOutputPath = "outputs/ToSignalsCount.xlsx";
+    final String sourceSignalOutputPath = "outputs/FromSignalsCount.xlsx";
+    final String feedSignalOutputPath = "outputs/ToSignalsCount.xlsx";
 
     // Input filepaths
     final String junctions_file_path = "src/main/resources/junctions_chennai.xlsx";
     final String congestion_history_file_path = "src/main/resources/congestion_history_jun_2023_to_jan_2024.xlsx";
 
-    @Test
-    public void writeFromSignalCounts() throws SpreadsheetReadException, IOException {
 
-        final SpreadsheetWriter ssw = new SpreadsheetWriter(fromSignalOutputPath);
+
+    @Test
+    public void writeSourceSignalCounts() throws SpreadsheetReadException, IOException {
+
+        this.writeSignalsCounts(true);
+
+    }
+
+    @Test
+    public void writeFeedSignalCounts() throws SpreadsheetReadException, IOException {
+
+        this.writeSignalsCounts(false);
+
+    }
+
+
+
+    private void writeSignalsCounts(boolean isSourceSignal) throws SpreadsheetReadException, IOException {
+
+        // Setting output file for Writer.
+        final String outputPath = isSourceSignal ? sourceSignalOutputPath : feedSignalOutputPath;
+        final SpreadsheetWriter sourceSignalWriter = new SpreadsheetWriter(outputPath);
+
 
         // List to write fields into Excel File
-        final List<JunctionsCount> fromJuncnsCountRecords = new ArrayList<JunctionsCount>();
-
-        // Reading Excel files
-        final XlsxReader reader = new XlsxReader();
-
-        final List<Junctions> junctions = reader.read(Junctions.class, new File(junctions_file_path));
-        final List<CongestionHistory> congHistoryList =
-                reader.read(CongestionHistory.class, new File(congestion_history_file_path));
-
+        final List<JunctionsCount> juncnsCountRecords = new ArrayList<JunctionsCount>();
 
         // Map to LOG counts (in console)
         final Map<String, Double> signalNoCountMap = new HashMap<String, Double>();
 
-        // Both files row data into Nested loops
-        for (Junctions jncn : junctions) {
-            if (Objects.isNull(jncn) || StringUtils.isBlank(jncn.getSignalNo())) {
-                continue;
-            }
-
-            double count = 0;
-            for (CongestionHistory congHistory : congHistoryList) { // Incrementing count on equality
-                if (Objects.nonNull(congHistory) && jncn.getSignalNo().equals(congHistory.getJunctionNo())) {
-                    count++;
-                    signalNoCountMap.put(jncn.getSignalNo(), count);
-                }
-            }
-            LOGGER.info("#FROM-Signal No. :: NoOf. Congestions = {} :: {}", jncn.getSignalNo(),
-                    signalNoCountMap.get(jncn.getSignalNo()));
-
-
-            fromJuncnsCountRecords.add(new JunctionsCount(jncn.getSignalNo(), jncn.getName(), count));
-        }
-
-        ssw.addSheet(JunctionsCount.class, fromJuncnsCountRecords, true);
-        ssw.write();
-    }
-
-
-    @Test
-    public void writeToSignalCounts() throws SpreadsheetReadException, IOException {
-
-        final SpreadsheetWriter ssw = new SpreadsheetWriter(toSignalOutputPath);
-
-        // List to write fields into Excel File
-        final List<JunctionsCount> toJuncnsCountRecords = new ArrayList<JunctionsCount>();
 
         // Reading Excel files
-        final XlsxReader reader = new XlsxReader();
-
-        final List<Junctions> junctions = reader.read(Junctions.class, new File(junctions_file_path));
+        final XlsxReader xlsxReader = new XlsxReader();
+        final List<Junctions> junctions = xlsxReader.read(Junctions.class, new File(junctions_file_path));
         final List<CongestionHistory> congHistoryList =
-                reader.read(CongestionHistory.class, new File(congestion_history_file_path));
+                xlsxReader.read(CongestionHistory.class, new File(congestion_history_file_path));
 
-        // Map to LOG counts (in console)
-        final Map<String, Double> signalNoCountMap = new HashMap<String, Double>();
 
-        // Both files row data into Nested loops
+        // Both files row data into Nested FOR loops
         for (Junctions jncn : junctions) {
+
             if (Objects.isNull(jncn) || StringUtils.isBlank(jncn.getSignalNo())) {
                 continue;
             }
+            
+            final String signalNo = jncn.getSignalNo();
 
-            double count = 0;
+            double jncnCount = 0;
+
             for (CongestionHistory congHistory : congHistoryList) { // Incrementing count on equality
-                if (Objects.nonNull(congHistory) && jncn.getSignalNo().equals(congHistory.getFeedJunctionNo())) {
-                    count++;
-                    signalNoCountMap.put(jncn.getSignalNo(), count);
+
+                if (isSourceSignal && Objects.nonNull(congHistory)
+                        && signalNo.equals(congHistory.getJunctionNo())) {
+                    jncnCount++;
+                }
+
+                if ((!isSourceSignal) && Objects.nonNull(congHistory)
+                        && signalNo.equals(congHistory.getFeedJunctionNo())) {
+                    jncnCount++;
                 }
             }
-            LOGGER.info("#To-Signal No. :: NoOf. Congestions = {} :: {}", jncn.getSignalNo(),
-                    signalNoCountMap.get(jncn.getSignalNo()));
+            // Setting count values of SignalNo. into Map & List
+            signalNoCountMap.put(signalNo, jncnCount);
+            LOGGER.info("Signal No. :: NoOf. Congestions = {} :: {}", signalNo, jncnCount);
 
-            toJuncnsCountRecords.add(new JunctionsCount(jncn.getSignalNo(), jncn.getName(), Double.valueOf(count)));
+            juncnsCountRecords.add(new JunctionsCount(signalNo, jncn.getName(), jncnCount));
         }
+        // Loop ends
 
-        ssw.addSheet(JunctionsCount.class, toJuncnsCountRecords, true);
-        ssw.write();
+
+        // Writing final List<JunctionsCount> into files
+        sourceSignalWriter.addSheet(JunctionsCount.class, juncnsCountRecords, true);
+        sourceSignalWriter.write();
     }
-
 }
-
